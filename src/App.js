a7b46665bad8dc1a74d7973d5355cd218f6bf29d
Ekaterina <nikitosia@yahoo.com>
2020-07-18 18:14:56 -0400

    header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+//import Constants, { API_URL } from "./constants";
+import Header from "./components/RootComp/Header";
+import Footer from "./components/RootComp/Footer";
+import {BrowserRouter} from "react-router-dom";
+import { Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">Hello world!</div>;
+  return (
+    <nav className="navigation">
+      <BrowserRouter>
+        <Route path="/" render={props => <Header {...props}/> } />
+      </BrowserRouter>
+
+      </nav>
+  
+  );
 }
 
 export default App;
---
+++ src/components/RootComp/Header.js
@@ -0,0 +1,49 @@
+import React from "react";
+import '../../Assets/theme/css/style.css';
+import '../../Assets/web/assets/mobirise-icons/mobirise-icons.css';
+import "../../Assets/mobirise/css/mbr-additional.css";
+import "../../Assets/dropdown/css/style.css";
+import "../../Assets/bootstrap/css/bootstrap.min.css";
+
+const Header = props => (
+    <section class="menu cid-qTkzRZLJNu" once="menu" id="menu1-0">
+        <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </button>
+            <div class="menu-logo">
+                <div class="navbar-brand">
+                    <span class="navbar-caption-wrap">
+                        <a class="navbar-caption text-white" href="/">
+                            HOMBOUND
+                        </a>
+                    </span>
+                </div>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
+                    <li class="nav-item">
+                        <a class="nav-link link text-white display-4" href="/">
+                        <span class="mbri-home mbr-iconfont mbr-iconfont-btn"></span>
+                        Contact us
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link link text-white display-4" href="/">
+                            <span class="mbri-users mbr-iconfont mbr-iconfont-btn"></span>
+                        Account
+                        </a>
+                    </li></ul>
+            </div>
+        </nav>
+    </section>
+)
+    
+
+export default Header;
+
