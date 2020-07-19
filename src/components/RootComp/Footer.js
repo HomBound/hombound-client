496e85f963e3393c3f1035341ef3039001e17880
Ekaterina <nikitosia@yahoo.com>
2020-07-19 00:50:38 -0400

    banner-footer

--- src/components/RootComp/Footer.js
+++ src/components/RootComp/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import '../../Assets/n/theme/css/style.css';
+import '../../Assets/theme/css/style.css';
 import '../../Assets/mobirise-icons/mobirise-icons.css';
 import "../../Assets/n/mobirise/css/mbr-additional.css";
 import "../../Assets/bootstrap/css/bootstrap.min.css";
