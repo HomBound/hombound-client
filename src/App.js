afb840ce3a38f16e4dd810271aa9c6da800bffcd
Roland <roland.aroche@gmail.com>
2020-07-19 12:58:09 -0400

    bug fix

--- src/App.js
+++ src/App.js
@@ -2,26 +2,23 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Route } from "react-router-dom";
 
-
-import Header from "./components/rootcomp/Header";
-import Footer from "./components/rootcomp/Footer";
+import Header from "./components/RootComp/Header/Header";
+import Footer from "./components/RootComp/Footer/Footer";
 import HomePage from "./components/homepage/HomePage";
-import ContacUs from "./components/contact/ContactUs";
-import AboutUs from "./components/about/AboutUs";
-import Account from "./components/account/Account";
+import ContacUs from "./components/Contact/ContactUs";
+import AboutUs from "./components/About/AboutUs";
+import Account from "./components/Account/Account";
 
 function App() {
   return (
-
-      <BrowserRouter>
-        <Route path="/" render={Header} />
-        <Route exact path="/" render={HomePage} />
-        <Route path="/ContactUs" render={ContacUs} />
-        <Route path="/AboutUs" render={AboutUs} />
-        <Route path="/Account" render={Account} />
-        <Route path="/" render={Footer} />
-      </BrowserRouter>
-  
+    <BrowserRouter>
+      <Route path="/" render={Header} />
+      <Route exact path="/" render={HomePage} />
+      <Route path="/ContactUs" render={ContacUs} />
+      <Route path="/AboutUs" render={AboutUs} />
+      <Route path="/Account" render={Account} />
+      <Route path="/" render={Footer} />
+    </BrowserRouter>
   );
 }
 
