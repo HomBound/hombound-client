92b8d996c1fb6acf8e1d4172121f2cd6c12bdb4d
Ekaterina <nikitosia@yahoo.com>
2020-07-18 20:35:45 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-//import Constants, { API_URL } from "./constants";
 import Header from "./components/RootComp/Header";
-import Footer from "./components/RootComp/Footer";
 import {BrowserRouter} from "react-router-dom";
 import { Route } from "react-router-dom";
 
