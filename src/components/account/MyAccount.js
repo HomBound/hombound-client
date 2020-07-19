9537ceea2cd0e164d6de0faca634594e38d0fc92
janeth <jarri023@fiu.edu>
2020-07-19 13:15:56 -0400

    login state

--- src/components/account/MyAccount.js
+++ src/components/account/MyAccount.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const MyAccount = () => {
+const MyAccount = (props) => {
   return (
     <div>
-      <h1>My Account</h1>
+      <h1>Hello, {props.user.firstName}</h1>
+      {console.log(props)}
     </div>
   );
 };
