b3faeb2ef109805c143df4c4027d27de14a3be8c
Roland <roland.aroche@gmail.com>
2020-07-19 00:37:58 -0400

    initial layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
 import Header from "./components/RootComp/Header";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Route } from "react-router-dom";
 
 function App() {
   return (
     <nav className="navigation">
       <BrowserRouter>
-        <Route path="/" render={props => <Header {...props}/> } />
+        <Route path="/" render={(props) => <Header {...props} />} />
       </BrowserRouter>
-
-      </nav>
-  
+    </nav>
   );
 }
 
