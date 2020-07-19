98878fccb71e15fcecafc7328045ecf15d45aaac
janeth <jarri023@fiu.edu>
2020-07-19 13:15:46 -0400

    user can now log in and have all their info

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./components/root/Header";
 import { BrowserRouter } from "react-router-dom";
 import { Route } from "react-router-dom";
@@ -6,17 +6,68 @@ import CreateAccount from "./components/account/CreateAccount";
 import MyAccount from "./components/account/MyAccount";
 import SignIn from "./components/account/SignIn";
 
-function App() {
-  return (
-    <nav className="navigation">
-      <BrowserRouter>
-        <Route path="/" render={(props) => <Header {...props} />} />
-        <Route path="/create-account" component={CreateAccount} />
-        <Route path="/sign-in" component={SignIn} />
-        <Route path="/my-account" component={MyAccount} />
-      </BrowserRouter>
-    </nav>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: false,
+      userId: null,
+      user: {},
+    };
+  }
+
+  componentWillMount() {
+    //pull from local storage
+    localStorage.getItem("currentUser") &&
+      this.setState({
+        user: JSON.parse(localStorage.getItem("currentUser")),
+      });
+    localStorage.getItem("userId") &&
+      this.setState({
+        userId: JSON.parse(localStorage.getItem("userId")),
+      });
+    localStorage.getItem("loggedIn") &&
+      this.setState({
+        loggedInStatus: JSON.parse(localStorage.getItem("loggedIn")),
+      });
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    //save to local storage
+    localStorage.setItem("currentUser", JSON.stringify(nextState.user));
+    localStorage.setItem("userId", JSON.stringify(nextState.userId));
+    localStorage.setItem("loggedIn", JSON.stringify(nextState.loggedIn));
+    localStorage.setItem("stateTime", Date.now()); //time-stamp
+  }
+
+  handleLoginStatus = (data) => {
+    this.setState({
+      loggedIn: true,
+      userId: data.id,
+      user: data,
+    });
+  };
+
+  render() {
+    return (
+      <nav className="navigation">
+        <BrowserRouter>
+          <Route path="/" render={(props) => <Header {...props} />} />
+          <Route path="/create-account" component={CreateAccount} />
+          <Route
+            path="/sign-in"
+            render={(props) => (
+              <SignIn {...props} handleLoginStatus={this.handleLoginStatus} />
+            )}
+          />
+          <Route
+            path="/my-account"
+            render={(props) => <MyAccount {...props} user={this.state.user} />}
+          />
+        </BrowserRouter>
+      </nav>
+    );
+  }
 }
 
 export default App;
