03d2ec0a3d89197f402b42e11c9aa3ab6e5bfe41
janeth <jarri023@fiu.edu>
2020-07-19 14:40:42 -0400

    account details

--- src/components/account/MyAccount.js
+++ src/components/account/MyAccount.js
@@ -21,10 +21,17 @@ const MyAccount = (props) => {
         )}
       </div>
       <div className="account--grid">
-        <h3>Account Details</h3>
-        <div>
-          {props.user.firstName} {props.user.lastName}
+        <h3 className="account--section-title">Account Details</h3>
+        <div className="account--details">
+          Name: {props.user.firstName} {props.user.lastName}
         </div>
+        <div className="account--details">Username: {props.user.username}</div>
+        <div className="account--details">Email: {props.user.email}</div>
+        <div className="account--details">
+          Address: {props.user.address} {props.user.city}, {props.user.state}
+        </div>
+        <h3 className="account--section-title">Payment Methods</h3>
+        {/* TODO: add payment details */}
       </div>
       {console.log(props)}
     </div>
