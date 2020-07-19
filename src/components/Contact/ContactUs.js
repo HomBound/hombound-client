0bfb2cd32309aa6c27666ef4cd31f86245435d17
Ekaterina <nikitosia@yahoo.com>
2020-07-19 13:07:21 -0400

    try13

--- src/components/Contact/ContactUs.js
+++ src/components/Contact/ContactUs.js
@@ -1,12 +1,4 @@
 import React from "react";
-import '../../Assets/mobirise-icons/mobirise-icons.css';
-
-import "../../Assets/bootstrap/css/bootstrap.min.css";
-import "../../Assets/bootstrap/css/bootstrap-grid.min.css";
-import "../../Assets/bootstrap/css/bootstrap-reboot.min.css";
-import "../../Assets/bootstrap/css/bootstrap.min.css";
-import '../../Assets/socicon/css/styles.css';
-import "../../Assets/theme/css/style1.css";
 
 const ContactUs = props => (
 
