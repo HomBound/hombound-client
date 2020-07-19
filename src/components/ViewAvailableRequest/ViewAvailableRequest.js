0bf7464c379ac2e85e92c5f3308b9b74cc2ceb07
Roland <roland.aroche@gmail.com>
2020-07-19 16:21:17 -0400

    roland incomplete ViewAvailableRequest.js

--- src/components/ViewAvailableRequest/ViewAvailableRequest.js
+++ src/components/ViewAvailableRequest/ViewAvailableRequest.js
@@ -1,30 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./ViewAvailableRequest.css";
-import useAvailableRequest from "../../hooks/useAvailableRequest";
+import { API_URL } from "../../constants";
 
-const ViewAvailableRequest = (props) => {
-  const requests = useAvailableRequest();
+class ViewAvailableRequest extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="items-container">
-      {requests
-        ? requests.map((request) => {
-            return (
-              <Link to={`/${request.id}`} className="item" key={request.id}>
-                <div
-                  className="item-img"
-                  style={{
-                    backgroundImage: "url('../../Assets/img/bg_card.jpeg')",
-                  }}
-                ></div>
-                <p className="item-name">{request.store}</p>
-              </Link>
-            );
-          })
-        : null}
-    </div>
-  );
-};
+    this.state = {
+      requests: [],
+      isLoading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+
+    fetch(`${API_URL}/requests/available`)
+      .then((response) => response.json())
+      .then((data) => this.setState({ requests: data, isLoading: false }));
+  }
+
+  render() {
+    const { requests, isLoading } = this.state;
+
+    if (isLoading) {
+      return <p>Loading ...</p>;
+    }
+
+    return (
+      <div className="items-container">
+        {requests
+          ? requests.map((request) => {
+              return (
+                <Link to={`/${request.id}`} className="item" key={request.id}>
+                  <div
+                    className="item-img"
+                    style={{
+                      backgroundImage: "url('../../Assets/img/bg_card.jpeg')",
+                    }}
+                  ></div>
+                  <p className="item-name">{request.store}</p>
+                </Link>
+              );
+            })
+          : null}
+      </div>
+    );
+  }
+}
 
 export default ViewAvailableRequest;
