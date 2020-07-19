7a273ba8557b4f4967820c2aeab264acf9e51e07
janeth <jarri023@fiu.edu>
2020-07-19 14:25:06 -0400

    account link to ready to help page

--- src/components/account/MyAccount.js
+++ src/components/account/MyAccount.js
@@ -1,9 +1,31 @@
 import React from "react";
+import "./Account.css";
+import { Link } from "react-router-dom";
 
 const MyAccount = (props) => {
+  const welcomeMessage = props.user.volunteer
+    ? "Thank you for helping us in the fight against Covid."
+    : "Wishing you get better soon.";
   return (
     <div>
-      <h1>Hello, {props.user.firstName}</h1>
+      <h1 className="account--header">Hello, {props.user.firstName}</h1>
+      <h2 className="account--welcome">{welcomeMessage}</h2>
+      <div className="account--options">
+        {props.user.volunteer ? (
+          <Link className="account--link">Ready To Help</Link>
+        ) : (
+          <div>
+            <Link>Create a Request</Link>
+            <Link>View My Requests</Link>
+          </div>
+        )}
+      </div>
+      <div className="account--grid">
+        <h3>Account Details</h3>
+        <div>
+          {props.user.firstName} {props.user.lastName}
+        </div>
+      </div>
       {console.log(props)}
     </div>
   );
