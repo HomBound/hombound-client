dece0646e5b0ba6f12fec801f84a4113f9219444
Ekaterina <nikitosia@yahoo.com>
2020-07-19 13:29:08 -0400

    try15

--- src/components/homepage/Banner.js
+++ src/components/homepage/Banner.js
@@ -15,21 +15,21 @@ const Banner = props => (
 
 
 <section 
-                class="header6 cid-s4p5fUANN9 mbr-fullscreen" 
+                className="header6 cid-s4p5fUANN9 mbr-fullscreen" 
                 data-bg-video="https://www.youtube.com/watch?v=rdFk-qjM3AU" 
                 id="header6-4">
                 <div 
-                    class="mbr-background-video-preview" 
+                    className="mbr-background-video-preview" 
                     style={{"background-size": 'cover', 'background-position': 'center center', 
                     'background-image': 'http://img.youtube.com/vi/rdFk-qjM3AU/maxresdefault.jpg'}}>
                 </div>
                 <div 
-                    class="mbr-background-video mb_YTPlayer isMuted" 
+                    className="mbr-background-video mb_YTPlayer isMuted" 
                     id="YTP_1595119813490" 
                     style={{'pointer-events': 'none'}}>
                 
                     <div 
-                        class="mbYTP_wrapper" 
+                        className="mbYTP_wrapper" 
                         id="wrapper_YTP_1595119813490" 
                         style={{'position': 'absolute', 'z-index': '0', 'min-width': '100%', 
                                'min-height': '100%', 'left': '0px', 'top': '0px', 'overflow': 'hidden', 'opacity': '1', 
@@ -38,7 +38,7 @@ const Banner = props => (
                     <iframe src='https://www.youtube.com/embed/rdFk-qjM3AU?modestbranding=1&mute=1&rel=0&autoplay=1&loop=1” frameborder=“0" allowfullscreen include></iframe>
 
 '
-                class="playerBox" 
+                className="playerBox" 
                 style={{'position': 'absolute', 'z-index': '0', 'width': '1467px', 'height': '825px', 
                         'top': '0px', 'left': '0px', 'overflow': 'hidden', 'opacity': '1', 'user-select': 'none',
                         'margin-top': '0px', 'margin-left': '-307px', 'max-width': 'initial', 
@@ -65,27 +65,27 @@ const Banner = props => (
         unselectable="on"
 />
                         <div 
-                            class="YTPOverlay" 
+                            className="YTPOverlay" 
                             style={{'position': 'absolute', 'top': '0px', 'left': '0px', 'width': '100%', 'height': '100%'}}>
                         </div>
                     </div>
                 </div>
                 <div
-                    class="mbr-overlay" 
+                    className="mbr-overlay" 
                     style={{'opacity': '0.1', 'background-color': 'rgb(35, 35, 35)'}}>
                 </div>
-                <div class="container">
-                    <div class="row justify-content-md-center">
-                        <div class="mbr-white col-md-10">
-                            <h1 class="mbr-section-title align-center mbr-bold pb-3 mbr-fonts-style display-1">
+                <div className="container">
+                    <div className="row justify-content-md-center">
+                        <div className="mbr-white col-md-10">
+                            <h1 className="mbr-section-title align-center mbr-bold pb-3 mbr-fonts-style display-1">
                             Together We Can<br/>Stay Home<br/>Save Lifes
                             </h1>
                         </div>
                     </div>
                 </div>
-                <div class="mbr-arrow hidden-sm-down" aria-hidden="true">
+                <div className="mbr-arrow hidden-sm-down" aria-hidden="true">
                     <a href="/">
-                        <i class="mbri-down mbr-iconfont"></i>
+                        <i className="mbri-down mbr-iconfont"></i>
                     </a>
                 </div>
                 
