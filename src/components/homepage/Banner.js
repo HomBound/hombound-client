00328d62d636d9461dee56498e1f678af8035e8a
Ekaterina <nikitosia@yahoo.com>
2020-07-19 16:25:00 -0400

    try again

--- src/components/homepage/Banner.js
+++ src/components/homepage/Banner.js
@@ -9,7 +9,6 @@ import "../../Assets/bootstrap/css/bootstrap-reboot.min.css";
 import "../../Assets/bootstrap/css/bootstrap.min.css";
 import '../../Assets/socicon/css/styles.css';
 import "../../Assets/theme/css/style1.css";
-import ZipHolder from "./ZipHolder";
 
 const Banner = props => (
             
